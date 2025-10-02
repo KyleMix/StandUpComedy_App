623e84a68a60c674089481649c02da78fae3828b
KyleMix <131567338+KyleMix@users.noreply.github.com>
2025-10-01 22:40:06 -0700

    Enable server actions support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const allowedOrigins = ["localhost:3000"];
+
+if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+  allowedOrigins.push(process.env.NEXT_PUBLIC_VERCEL_URL);
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins
+    }
+  }
+};
 
 module.exports = nextConfig;
